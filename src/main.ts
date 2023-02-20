7a19c476018f1cfcbc98dedf95d1b87ef2be8caf
behrad kazemi <behradkzm@gmail.com>
2023-02-20 12:03:16 +0330

    added otlp auto-instrumentations  monitoring

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,11 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './utils/interceptors/error-handling';
 import validationOptions from './utils/common/validation-options';
 import { urlencoded, json } from 'express';
+import { setupAutoInstrumenting } from './utils/monitoring/tracing.otlp';
 
 async function bootstrap() {
   
+  setupAutoInstrumenting();
   const app = await NestFactory.create(AppModule, { cors: true });
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   const configService = app.get(ConfigService);
